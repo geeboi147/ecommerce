12767336d71e2e046db38063766ea25f357ede12
geeboi147 <oguntoye.oluwaseyi@gmail.com>
2024-03-21 10:24:58 +0100

    ecommerce

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+<div className="descriptionbox-navigator">
+   <div className="descriptionbox-nav-box">Description</div> 
+   <div className="descriptionbox-nav-box fade">Reviews (122)</div> 
+</div>
+<div className="descriptionbox-description">
+    <p>what is an e-commerce website Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi, nisi maiores blanditiis tenetur nostrum asperiores eos est neque inventore, fuga nulla fugiat temporibus quam non dignissimos odit, mollitia natus!</p>
+    <p>
+        what does it display
+    </p>
+</div>
+    </div>
+  )
+}
+
+export default DescriptionBox
